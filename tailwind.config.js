74d81f9a99a92b4a1503e3c9ea8a2ff5bacc6d4f
Josh Kennedy <josh.kennedy@daveramsey.com>
2020-12-29 06:58:28 -0600

    Update to Tailwind 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ["./public/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {},
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 };
